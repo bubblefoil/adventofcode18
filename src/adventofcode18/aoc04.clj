c4de2d830c7a33f0dcc1e17dcfb09289c838d766
bubblefoil <go.holyal@gmail.com>
2018-12-06 19:32:17 +0100

    Day 4, part I

--- src/adventofcode18/aoc04.clj
+++ src/adventofcode18/aoc04.clj
@@ -1,49 +1,24 @@
 (ns adventofcode18.aoc04
   (:require [clojure.java.io :as io]
-            [adventofcode18.file :as f])
-  (:import (java.time.format DateTimeFormatter)
-           (java.time.temporal TemporalAccessor)
-           (java.time LocalDateTime)))
+            [adventofcode18.file :as f]))
 
 (defn read-ids [res-name]
   (f/lines-of (io/resource res-name)))
 
 (def shifts (read-ids "./aoc04.txt"))
 
-(defn read-shift-record
-  "Parses a shift record."
+(defn read-minute
+  "Parses the minute from a shift record."
   [shift]
-  (let [ts (subs shift 1 17)
-        minute (f/to-int (subs shift 15 17))
-        action (subs shift 19)]
-    {:ts ts :minute minute :action action}))
-
-(defn parse-instant
-  [instant]
-  (let [df (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm")]
-    (LocalDateTime/parse instant df)))
-
-(defn align-to-midnight
-  "Aligns an instant to the closest midnight."
-  [^LocalDateTime temporal]
-  (-> temporal
-      (.plusHours 1)
-      (.toLocalDate)))
-
-(defn get-day
-  [instant]
-  (-> instant
-      (parse-instant)
-      (align-to-midnight)))
+  (f/to-int (subs shift 15 17)))
 
 (defn minutes-asleep
   [guard-awakeness-records]
   (->> guard-awakeness-records
-       (map :minute)
+       (map read-minute)
        (partition 2)
        (map #(apply range %))
        (flatten)))
-;(set)))
 
 (def guard-id-pattern (re-pattern #"#(\d+)"))
 
@@ -51,63 +26,70 @@
   [str]
   (f/to-int (second (re-find guard-id-pattern str))))
 
-(defn process-shift
-  "Transforms grouped shift records."
-  [[[shift-beginning] guard-awakeness-records]]
-  {:day            (get-day (:ts shift-beginning))
-   :guard          (parse-guard-id (:action shift-beginning))
-   :minutes-asleep (minutes-asleep guard-awakeness-records)})
+(defmulti process-shift-record
+          (fn [_ record] (subs record 19 24)))
 
-(defn is-shift-change
-  "Returns true if given record is a beginning of a new shift."
-  [record]
-  (clojure.string/starts-with? (:action record) "Guard"))
+(defmethod process-shift-record "Guard" [shifts record]
+  (let [guard-id (parse-guard-id record)]
+    (-> shifts
+        (assoc :current-guard guard-id)
+        (update :shifts (fn [guard-shifts]
+                          (merge {guard-id {:guard guard-id :actions []}} guard-shifts))))))
+
+(defmethod process-shift-record :default [shifts record]
+  (let [guard-id (:current-guard shifts)]
+    (update-in shifts [:shifts guard-id :actions]
+               (fn [actions]
+                 (conj actions record)))))
+
+(defn update-values
+  [map f]
+  (reduce-kv (fn [m k v] (assoc m k (f v))) {} map))
+
+(defn max-by
+  [f coll]
+  (reduce (fn [x y] (max-key f x y)) coll))
+
+(defn process-shift-actions
+  "Transforms grouped shift records."
+  [{guard-awakeness-records :actions}]
+  {:minutes-asleep (minutes-asleep guard-awakeness-records)})
+
+(defn actions-to-minutes-asleep
+  [shifts]
+  (update-values shifts process-shift-actions))
 
 (defn read-shifts
   "Parses and sorts guard shift records."
   [shifts]
   (->> shifts
        (sort)
-       (map read-shift-record)
-       (partition-by is-shift-change)
-       (partition 2)
-       (map process-shift)
-       (group-by :guard)))
-
-(defn count-minutes
-  [guard-shifts]
-  (->> guard-shifts
-       (map :minutes-asleep)
-       (map count)
-       (reduce +)))
+       (reduce process-shift-record {:shifts {} :current-guard nil})
+       (:shifts)
+       (actions-to-minutes-asleep)))
 
 (defn find-drowsiest-minute
   "Returns a vector containing the number of the guard's drowsiest minute
    and total count of days on which the guard was asleep during that minute."
-  [guard-shifts]
-  (->> guard-shifts
-       (map :minutes-asleep)
-       (map vec)
-       (reduce concat)
+  [minutes-asleep]
+  (->> minutes-asleep
        (frequencies)
-       (reduce #(max-key val %1 %2))))
+       (max-by val)
+       ;get the key from the max entry
+       (first)))
 
 (defn find-drowsiest-guard
   "Finds the guard who sleeps the most."
   [shifts]
   (->> shifts
-       (reduce-kv
-         (fn [acc k v] (conj acc {:guard k :minutes-asleep-total (count-minutes v)}))
-         [])
-       (reduce (fn [acc x] (max-key :minutes-asleep-total acc x)))))
+       (max-by (comp count :minutes-asleep val))))
 
 (defn drowsiness-key
   "Multiplies if of the guard who sleeps the most by the minute during which they sleep most often."
   [shifts]
-  (let [the-drowsiest (find-drowsiest-guard shifts)
-        drowsiest-minute (find-drowsiest-minute (get shifts (:guard the-drowsiest)))]
-    (* (:guard the-drowsiest) (first drowsiest-minute))))
-
+  (let [[id {minutes-asleep :minutes-asleep}] (find-drowsiest-guard shifts)
+        drowsiest-minute (find-drowsiest-minute minutes-asleep)]
+    (* id drowsiest-minute)))
 
 (comment
   (def example1 ["[1518-11-01 00:00] Guard #10 begins shift"
@@ -115,6 +97,14 @@
                  "[1518-11-01 00:25] wakes up"
                  "[1518-11-01 00:30] falls asleep"
                  "[1518-11-01 00:55] wakes up"])
+  (def example2 ["[1518-06-28 00:03] Guard #1033 begins shift"
+                 "[1518-03-29 23:58] Guard #1987 begins shift"
+                 "[1518-03-30 00:34] falls asleep"
+                 "[1518-03-30 00:35] wakes up"
+                 "[1518-03-30 00:44] falls asleep"
+                 "[1518-03-30 00:47] wakes up"
+                 "[1518-03-30 00:51] falls asleep"
+                 "[1518-03-30 00:53] wakes up)"])
   (def example ["[1518-11-01 00:00] Guard #10 begins shift"
                 "[1518-11-01 00:05] falls asleep"
                 "[1518-11-01 00:25] wakes up"
@@ -132,17 +122,10 @@
                 "[1518-11-05 00:03] Guard #99 begins shift"
                 "[1518-11-05 00:45] falls asleep"
                 "[1518-11-05 00:55] wakes up"])
-  (def awakeness [{:ts "1518-11-21 00:00", :minute 0, :action "falls asleep"}
-                  {:ts "1518-11-21 00:38", :minute 38, :action "wakes up"}
-                  {:ts "1518-11-21 00:43", :minute 43, :action "falls asleep"}
-                  {:ts "1518-11-21 00:46", :minute 46, :action "wakes up"}])
-  (minutes-asleep awakeness)
-  (re-find (re-pattern #"\[()]"))
-  (read-shift-record (first example))
   (read-shifts example1)
   (read-shifts example)
   (read-shifts shifts)
-  (def the-drowsiest (find-drowsiest-guard (read-shifts shifts))) ;240
+  (def the-drowsiest (find-drowsiest-guard (read-shifts shifts))) ;
   (find-drowsiest-minute (get (read-shifts shifts) (:guard the-drowsiest))) ;
   (drowsiness-key (read-shifts example))                    ;
   ;Part II solution
